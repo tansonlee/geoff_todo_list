5f73c5ceb5f5779c1d866e4f5f2d1b00ff8a3598
Tanson Lee <leetanson@gmail.com>
2023-07-08 00:38:11 -0400

    More logs

--- pages/api/todos/list.ts
+++ pages/api/todos/list.ts
@@ -7,17 +7,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		if (req.method !== 'GET') {
 			return res.status(405).json({ error: 'Method not allowed, only GET is allowed.' });
 		}
+		console.log(1);
 
 		// Write the actual logic
 		// 1. Extract the data from the database.
 		const client = await clientPromise;
+		console.log(2);
 		const myDB = client.db('Cluster1');
+		console.log(3);
 		const myColl = myDB.collection('todo');
+		console.log(4);
 		const todos = await myColl.find({}).toArray();
+		console.log(5);
 		// 2. Return the data.
 		return res.status(200).json({ todos: todos });
 		//List the TODO
 	} catch (e) {
+		console.log(6);
 		return res.status(500).json({ error: e });
 	}
 }
